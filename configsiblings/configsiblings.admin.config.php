fd6feda356d3fa67b9da0f61a23fcbac68f079f5
Twiebie <twiebie@twiebie.com>
2013-06-23 21:59:36 +0200

    Icons in config row, fix for paused plugins

--- configsiblings/configsiblings.admin.config.php
+++ configsiblings/configsiblings.admin.config.php
@@ -8,20 +8,36 @@ Hooks=admin.config.edit.tags
 defined('COT_CODE') or die('Wrong URL');
 
 $siblings = $db->query("
-	SELECT DISTINCT(config_cat) FROM $db_config
+	SELECT DISTINCT config_cat, pl_title FROM $db_config
+	LEFT JOIN $db_plugins ON config_cat = pl_code
 	WHERE config_owner = ? AND config_type != ?
 	ORDER BY config_cat ASC
-", array($o, COT_CONFIG_TYPE_HIDDEN))->fetchAll();
+	", array($o, COT_CONFIG_TYPE_HIDDEN))->fetchAll();
+
 if ($siblings)
 {
 	foreach ($siblings as $sibling)
 	{
-		if ($o == 'core') $name = $L["core_{$sibling['config_cat']}"];
-		if ($o == 'module') $name = $cot_modules[$sibling['config_cat']]['title'];
-		if ($o == 'plug') $name = $cot_plugins_enabled[$sibling['config_cat']]['title'];
+        switch ($o)
+        {
+            case 'core';
+                $name = $L["core_{$sibling['config_cat']}"];
+                $icofile = $cfg['system_dir'] . '/admin/img/cfg_' . $sibling['config_cat'] . '.png';
+                $sibling_ext['icon'] = (file_exists($icofile)) ? $icofile : '';
+                break;
+            case 'module':
+				$name = $sibling['pl_title'];
+				$sibling_ext = cot_get_extensionparams($sibling['config_cat'], true);
+                break;
+            case 'plug':
+				$name = $sibling['pl_title'];
+				$sibling_ext = cot_get_extensionparams($sibling['config_cat'], false);
+                break;
+        }
 		$t->assign(array(
-			'ADMIN_CONFIG_SIBLING_URL' => cot_url('admin', "m=config&n=edit&o=$o&p={$sibling['config_cat']}"),
-			'ADMIN_CONFIG_SIBLING_NAME' => $name,
+			'ADMIN_CONFIG_SIBLING_URL'    => cot_url('admin', "m=config&n=edit&o=$o&p={$sibling['config_cat']}"),
+			'ADMIN_CONFIG_SIBLING_NAME'   => $name,
+			'ADMIN_CONFIG_SIBLINGS_ICO'   => $sibling_ext['icon'],
 			'ADMIN_CONFIG_SIBLING_ACTIVE' => ($sibling['config_cat'] == $p)
 		));
 		$t->parse('MAIN.EDIT.ADMIN_CONFIG_SIBLINGS.ADMIN_CONFIG_SIBLINGS_ROW');
@@ -30,5 +46,3 @@ if ($siblings)
 	$t->assign('ADMIN_CONFIG_OWNER', $owners[$o]);
 	$t->parse('MAIN.EDIT.ADMIN_CONFIG_SIBLINGS');
 }
-
-?>
